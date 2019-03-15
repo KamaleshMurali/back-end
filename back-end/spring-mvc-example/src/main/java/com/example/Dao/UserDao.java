69dccb2475e8a1c50491b52f633de71fe8769ea3
OFS1\kamalesh.murali <kamalesh.murali@object-frontier.com>
2019-03-15 15:31:53 +0530

    userDao and userService has been created.

---
+++ back-end/spring-mvc-example/src/main/java/com/example/Dao/UserDao.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.example.Dao;
+
+/**
+ * @author kamalesh.murali
+ * @since Mar 12, 2019
+ */
+public class UserDao {
+
+}
---
+++ back-end/spring-mvc-example/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.example.service;
+
+/**
+ * @author kamalesh.murali
+ * @since Mar 12, 2019
+ */
+public class UserService {
+
+}
